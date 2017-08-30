4b95eae2eb002f6de6c364b93bfca46ec179b7e3
farreldev <rickyalext@gmail.com>
2017-08-30 14:26:39 +0700

    Initial Commit

---
+++ Core/script/scripts.js
@@ -0,0 +1,67 @@
+(function($){
+    "use strict"
+    
+    var app = (function() {
+        
+        var $siteContainer = $('.site-container'),
+            $isHome = $siteContainer.find('div').hasClass('homepage'),
+            $isPageSeacrh = $siteContainer.find('div').hasClass('info-search-results'),
+            $isSubHead = $siteContainer.find('div').hasClass('.sub-head-child-pages'),
+            $subHead =  $siteContainer.find('.sub-head-child-pages');
+            
+            ($isHome) ? $('.top-center').hide() : $('.top-center').show();
+            ($isPageSeacrh) ? $subHead.css({marginTop: '-83px'}) : '';
+        
+            $('.humburger-btn').on('click', function() {
+                if($('.inputSearch').hasClass('searchOpen')) {
+                    $(this).toggleClass('humTrigger');
+                    $('.navigasi').toggleClass('mnuOpen');
+                    $('.inputSearch').removeClass('searchOpen'); 
+                } else {
+                    $(this).toggleClass('humTrigger');
+                    $('.navigasi').toggleClass('mnuOpen');
+                }
+            });
+            
+            $('.cari-btn').on('click', function() {
+                if($('.navigasi').hasClass('mnuOpen') && $('.humburger-btn').hasClass('humTrigger')) {
+                    $('.navigasi').removeClass('mnuOpen');
+                    $('.humburger-btn').removeClass('humTrigger');
+                    $('.inputSearch').toggleClass('searchOpen');
+                } else {
+                    $('.inputSearch').toggleClass('searchOpen');
+                }
+                            
+
+                if($isPageSeacrh) {
+                    $('.info-search-results').siblings('.sub-head-child-pages')
+                    .animate({
+                        marginTop: 0,
+                        paddingBottom: '110px'
+                    }, 300, function() {
+                        if(!$('.inputSearch').hasClass('searchOpen')) {
+                            $('.sub-head-child-pages').animate({
+                                paddingBottom: 0,
+                                marginTop: '-83px'
+                            }, 300)
+                        }
+                    });
+                }
+            })
+
+            $('#txtSearch').on('input', function() {
+                if($(this).val() !== "") {
+                    $('.inputSearch a').show();
+                } else {
+                    $('.inputSearch a').hide();
+                }
+            })
+
+            $('.inputSearch').on('click', 'a', function(event) {
+                event.preventDefault();
+                $('#txtSearch').val('').next().hide();
+                
+            })
+    })();
+    
+})(jQuery);
